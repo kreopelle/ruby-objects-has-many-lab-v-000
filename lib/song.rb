e17c00d01c98a7473cb86e677048595d2a16776d
Kayla Reopelle <kreopelle@gmail.com>
2018-05-20 16:37:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,18 +3,14 @@ class Song
 
   def artist
     @artist = nil
-  end 
+  end
 
   def initialize(name)
     @name = name
   end
 
   def artist_name
-    if :artist
       self.artist.name
-    else
-      nil
-    end 
   end
 
 end
